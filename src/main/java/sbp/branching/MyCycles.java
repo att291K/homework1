b562e5d3e9ced1ad82323ee766c4f3a6cbc18db4
att291K <att291@mail.ru>
2025-10-12 20:13:23 +0300

    тесты первой работы

--- src/main/java/sbp/branching/MyCycles.java
+++ src/main/java/sbp/branching/MyCycles.java
@@ -22,8 +22,11 @@ public class MyCycles
      */
     public void cycleForExample(int iterations, String str)
     {
-        /*
-        ...
+        /**
+         * Метод с использованием for, на каждой итерации вызывается функция Utils#utilFunc1()
+        при положительном ответе вызывается Utils#link(String)}
+         * @param iterations - количество итераций
+         * @param str - строка для вывода через утилиту {@link Utils}
          */
 
         for (int i = 0; i < iterations; i++) {
@@ -47,15 +50,26 @@ public class MyCycles
      */
     public void cycleWhileExample(int iterations, String str)
     {
-        /*
-        ...
+        /**
+         * метод с использованием while c шагом 3, на каждой итерации вызывается функция Utils#utilFunc1()
+         * при положительном ответе вызывается Utils#link
+         * @param iterations - количество итераций
+         * @param str - строка для вывода через утилиту {@link Utils}
          */
+        //System.out.println(iterations);
+        while (iterations > 0) {
+            if (utils.utilFunc1(str)) {
+                utils.link(str);
+            }
+            iterations = iterations-3;
+        }
+
     }
 
     /**
      * Необходимо написать реализацию метода с использованием while()
-     -     * Метод должен вызвать Utils#utilFunc1() на каждой итерации
-     -     * Реализация Utils#utilFunc1() неизвестна
+     +     * Метод должен вызвать Utils#utilFunc1() на каждой итерации
+     +     * Реализация Utils#utilFunc1() неизвестна
      +     * Метод должен вызвать Utils#utilFunc1() на каждой итерации
      +     * Реализация Utils#utilFunc1() неизвестна
      * Должна присутствовать проверка возврщаемого значения
@@ -66,8 +80,19 @@ public class MyCycles
      */
     public void cycleDoWhileExample(int from, int to, String str)
     {
-        /*
-        ...
+        /**
+         * метод с использованием do while, на каждой итерации вызывается функция Utils#utilFunc1()
+        при положительном ответе вызывается Utils#link
+         * @param from - начальное значение итератора
+         * @param to - конечное значение итератора
+         * @param str - строка для вывода через утилиту {@link Utils}
          */
+        int iterations = from;
+        do {
+            if (utils.utilFunc1(str)) {
+                utils.link(str);
+            }
+            iterations++;
+        }while (iterations <= to);
     }
 }
---
+++ src/test/java/sbp/branching/MyBranchingTest.java
@@ -0,0 +1,126 @@
+package sbp.branching;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sbp.common.Utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.mockito.ArgumentMatchers.anyString;
+
+public class MyBranchingTest {
+
+    /**
+     * Проверка на 0 при Utils#utilFunc2() возвращающем true
+     */
+    @Test
+    public void maxInt_Test()
+    {
+        final int int1 = 2;
+        final int int2 = 5;
+
+        Utils utilsMock = Mockito.mock(Utils.class);
+
+        Mockito.when(utilsMock.utilFunc1(anyString())).thenReturn(true);
+        Mockito.when(utilsMock.utilFunc2()).thenReturn(true);
+
+        MyBranching myBranching = new MyBranching(utilsMock);
+        int rez = myBranching.maxInt(int1, int2);
+        Assertions.assertEquals(0,rez);
+
+        //Mockito.verify(utilsMock, Mockito.times(1)).utilFunc2();
+    }
+
+    /**
+     * Поверка на результат выполнения метода в зависимости от возможных реализаций {@link Utils}
+     */
+    @Test
+    public void ifElseExample_Test()
+    {
+        Utils utilsMock = Mockito.mock(Utils.class);
+        Mockito.when(utilsMock.utilFunc2()).thenReturn(false);
+
+        MyBranching myBranching = new MyBranching(utilsMock);
+        boolean res = myBranching.ifElseExample();
+        Assertions.assertTrue(true);
+    }
+
+    /**
+     * Проверка количества обращений к {@link Utils}
+     */
+    @Test
+    public void switchExample_Test0()
+    {
+        final int iteration = 0;
+
+        Utils utilsMock = Mockito.mock(Utils.class);
+        Mockito.when(utilsMock.utilFunc1(anyString())).thenReturn(false);
+        Mockito.when(utilsMock.utilFunc2()).thenReturn(true);
+
+        MyBranching myBranching = new MyBranching(utilsMock);
+        myBranching.switchExample(iteration);
+
+        Mockito.verify(utilsMock, Mockito.times(1)).utilFunc1(anyString());
+        Mockito.verify(utilsMock, Mockito.times(1)).utilFunc2();
+    }
+    @Test
+    public void switchExample_Test1()
+    {
+        final int iteration = 1;
+
+        Utils utilsMock = Mockito.mock(Utils.class);
+        Mockito.when(utilsMock.utilFunc1(anyString())).thenReturn(false);
+        Mockito.when(utilsMock.utilFunc2()).thenReturn(false);
+
+        MyBranching myBranching = new MyBranching(utilsMock);
+        myBranching.switchExample(iteration);
+
+        Mockito.verify(utilsMock, Mockito.times(1)).utilFunc1(anyString());
+        Mockito.verify(utilsMock, Mockito.times(1)).utilFunc2();
+    }
+    @Test
+    public void switchExample_Test2()
+    {
+        final int iteration = 2;
+
+        Utils utilsMock = Mockito.mock(Utils.class);
+        Mockito.when(utilsMock.utilFunc1(anyString())).thenReturn(false);
+        Mockito.when(utilsMock.utilFunc2()).thenReturn(true);
+
+        MyBranching myBranching = new MyBranching(utilsMock);
+        myBranching.switchExample(iteration);
+
+        Mockito.verify(utilsMock, Mockito.times(0)).utilFunc1(anyString());
+        Mockito.verify(utilsMock, Mockito.times(1)).utilFunc2();
+    }
+    @Test
+    public void switchExample_Test()
+    {
+        final int iteration = 0;
+
+        Utils utilsMock = Mockito.mock(Utils.class);
+        Mockito.when(utilsMock.utilFunc1(anyString())).thenReturn(false);
+        Mockito.when(utilsMock.utilFunc2()).thenReturn(true);
+
+        int off = 0;
+        for(int i = 0;i<5;i++){
+            System.out.println("i="+i);
+            MyBranching myBranching = new MyBranching(utilsMock);
+            myBranching.switchExample(i);
+            int iter = i;
+
+            if (i==2) {
+                off++;
+            }
+
+            Mockito.verify(utilsMock, Mockito.times(1+iter-off)).utilFunc1(anyString());
+            Mockito.verify(utilsMock, Mockito.times(1+iter)).utilFunc2();
+        }
+
+    }
+}
